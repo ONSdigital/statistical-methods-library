26d02d78e1c69e3c33be8e7352b1e975154d8dc1
Adam Thompson <arthompson1990@gmail.com>
2021-05-25 14:29:52 +0100

    Add spark session fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,15 @@
-import pytest, os, json, csv
+import pytest
+from pyspark.sql import SparkSession
+
+
+@pytest.fixture(scope='session')
+def spark_test_session():
+    """
+    Creates a Spark session to be used throughout all tests.
+    """
+    return (SparkSession.builder
+        .appName("tests")
+        .master("local")
+        .enableHiveSupport()
+        .getOrCreate()
+    )
