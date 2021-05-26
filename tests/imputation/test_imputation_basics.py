d36e473e631dd4a08c799ce34d2ef70f1d24c657
Dom Ford <dom.ford@ons.gov.uk>
2021-05-26 17:23:17 +0100

    More basic tests

--- tests/imputation/test_imputation_basics.py
+++ tests/imputation/test_imputation_basics.py
@@ -22,13 +22,13 @@ params = (
 # ====================================================================================
 # --------------- TESTING TEMPLATE ---------------------------
 # ====================================================================================
-# --- Test fails with type error if no input ---
 # --- Test type validation on the input dataframe(s) ---
-# --- Test type validation on the target column lists(s) ---
 # --- Test if cols missing from input dataframe(s) ---
+# --- Test if any run-time params are null ---
 # --- Test if output is a dataframe (or the expected type)---
 # --- Test if output contents is as expected, both new columns and data content ---
 # --- Test any other error based outputs ---
+
 # IMPORTANT:
 # 1) If the test contains any form of condition or loop, you must test the logical
 #    branches to ensure that each assert is actually being performed.
@@ -38,19 +38,19 @@ params = (
 #    Instead, follow the existing templates to add conditional tests.
 # 4) If you load the test data in for each test rather than as a module level
 #    constant, you can amend data in the tests without needing new test data.
+# 5) Don't test for python language errors. :)
 # ====================================================================================
-
-
-# --- Test fails with type error if no input ---
 
 
 # --- Test type validation on the input dataframe(s) ---
 
-
-# --- Test type validation on the target column lists(s) ---
+def test_dataframe_not_a_dataframe():
+    with pytest.raises(imputation.ValidationError):
+        imputation.imputation('not_a_dataframe', *params)
 
 
 # --- Test if cols missing from input dataframe(s) ---
+
 def test_dataframe_column_missing(fxt_spark_session):
     test_dataframe = fxt_spark_session.read.csv(
         'tests/imputation/fixture_data/test_dataframe_returned.csv')
@@ -59,9 +59,26 @@ def test_dataframe_column_missing(fxt_spark_session):
         imputation.imputation(bad_dataframe, *params)
 
 
+# --- Test if params null ---
+
+def test_params_blank(fxt_spark_session):
+    test_dataframe = fxt_spark_session.read.csv(
+        'tests/imputation/fixture_data/test_dataframe_returned.csv')
+    bad_params = (
+        reference_col,
+        period_col,
+        strata_col,
+        "",
+        auxiliary_col,
+        output_col,
+        marker_col
+    )
+    with pytest.raises(imputation.ValidationError):
+        imputation.imputation(test_dataframe, *bad_params)
+
+
 # --- Test if output is a dataframe (or the expected type)---
 
-# noinspection PyMethodMayBeStatic
 def test_dataframe_returned(fxt_spark_session):
     test_dataframe = fxt_spark_session.read.csv(
         'tests/imputation/fixture_data/test_dataframe_returned.csv')
