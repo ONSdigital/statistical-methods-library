8d15a84155f5462114ff8e9117ac04b8f9ec23fa
Adam Thompson <arthompson1990@gmail.com>
2021-05-28 17:30:55 +0100

    display() to show() in ratio calculation test

--- tests/imputation/test_ratio_calculation.py
+++ tests/imputation/test_ratio_calculation.py
@@ -80,4 +80,4 @@ def test_ratios_as_expected(fxt_spark_session):
     ret_val = imputation.imputation(test_dataframe, *params)
     # perform action on the dataframe to trigger lazy evaluation
     _row_count = ret_val.count()
-    ret_val.display()
+    ret_val.show()
