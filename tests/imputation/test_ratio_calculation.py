cb44cb7cd38905c2a67e71834480c662ce394217
Dom Ford <dom.ford@ons.gov.uk>
2021-06-01 13:12:30 +0100

    refactor calc ratio test

--- tests/imputation/test_ratio_calculation.py
+++ tests/imputation/test_ratio_calculation.py
@@ -77,13 +77,11 @@ def test_new_columns_created(fxt_spark_session):
 
 def test_calculated_ratios_as_expected(fxt_spark_session, capsys):
     test_dataframe = load_test_csv(fxt_spark_session, "test_ratio_calculation_input.csv")
-    ret_val = imputation.imputation(test_dataframe, *params)
-    # perform action on the dataframe to trigger lazy evaluation
-    assert ret_val.count() > 0
-    # Disable output capturing so we can see the contents of the returned
-    # dataframe
     with capsys.disabled():
+        ret_val = imputation.imputation(test_dataframe, *params)
+        # perform action on the dataframe to trigger lazy evaluation
+        assert ret_val.count() > 0
+        # Disable output capturing so we can see the contents of the
+        # returned dataframe
         ret_val.show()
-
-
 
