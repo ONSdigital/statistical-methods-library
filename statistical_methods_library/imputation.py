25ba8898f4f50d0988e6403d0cb2768b218d8543
Adam Thompson <arthompson1990@gmail.com>
2021-06-03 17:52:05 +0100

    Hopefully more efficient version of the construction algorithm

--- statistical_methods_library/imputation.py
+++ statistical_methods_library/imputation.py
@@ -403,25 +403,27 @@ def imputation(
         period_df = filtered_df.select("period").distinct()
         ref_df = filtered_df.select("ref").distinct().persist()
         construction_union_df = None
-        for period_val in period_df.toLocalIterator():
-            for ref_val in ref_df.toLocalIterator():
-                if filtered_df.filter(
-                    (col("ref") == ref_val["ref"])
-                    & (col("period") == calculate_previous_period(
-                        period_val["period"]))
+        for ref_val in ref_df.toLocalIterator():
+            ref_filtered_df = filtered_df.filter(col("ref") == ref_val["ref"]
+            ).persist()
+            for period_val in period_df.toLocalIterator():
+                if ref_filtered_df.filter(
+                    (col("period") == calculate_previous_period(
+                        period_val["period"])
+                    )
                 ).count() > 0:
                     # We have a previous period so don't construct.
                     continue
 
-                calculation_df = filtered_df.filter(
-                    (col("ref") == ref_val["ref"])
-                    & (col("period") == period_val["period"])
+                calculation_df = ref_filtered_df.filter(
+                    col("period") == period_val["period"]
                 ).withColumn(
                     "constructed_output",
                     col("aux") * col("construction")
                 ).withColumn(
                     "constructed_marker", lit(MARKER_CONSTRUCTED)
                 )
+
                 if construction_union_df is None:
                     construction_union_df = calculation_df.persist()
                 else:
