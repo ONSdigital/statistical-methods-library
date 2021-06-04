2448cdffaf64817be1fd7d41a65358635f049f33
Adam Thompson <arthompson1990@gmail.com>
2021-06-04 13:15:38 +0100

    allow csv loading fixture to take an argument by returning a load function

--- tests/imputation/conftest.py
+++ tests/imputation/conftest.py
@@ -57,18 +57,25 @@ params = (
 )
 
 @pytest.fixture
-def fxt_load_test_csv(fxt_spark_session, filename):
-    path = "tests/imputation/fixture_data/"
-    filepath = os.path.join(path, filename)
-    test_dataframe = fxt_spark_session.read.csv(filepath, header=True)
-    select_col_list = []
-    for dataframe_col in dataframe_columns:
-        if dataframe_col in test_dataframe.columns:
-            select_col_list.append(
-                col(dataframe_col).cast(dataframe_types[dataframe_col])
-            )
+def fxt_load_test_csv(fxt_spark_session):
+    # We don't want to yield the function because we're explicitly using it to
+    # be called as a function in our tests and thus this fixture would have no
+    # access to its return value. We do this so that our loader can be
+    # passed a file name.
+    def load(filename):
+        path = "tests/imputation/fixture_data/"
+        filepath = os.path.join(path, filename)
+        test_dataframe = fxt_spark_session.read.csv(filepath, header=True)
+        select_col_list = []
+        for dataframe_col in dataframe_columns:
+            if dataframe_col in test_dataframe.columns:
+                select_col_list.append(
+                    col(dataframe_col).cast(dataframe_types[dataframe_col])
+                )
 
-    yield test_dataframe.select(select_col_list)
+        return test_dataframe.select(select_col_list)
+
+    return load
 
 @pytest.fixture(scope='session')
 def fxt_spark_session():
