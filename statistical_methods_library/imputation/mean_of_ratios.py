84a52626b53fe0de81df69dead74fd913db97c1f
Adam Thompson <arthompson1990@gmail.com>
2023-03-01 14:22:41 +0000

    New growth ratio calculations for mean of ratios

--- statistical_methods_library/imputation/mean_of_ratios.py
+++ statistical_methods_library/imputation/mean_of_ratios.py
@@ -10,61 +10,20 @@ from . import engine
 
 def impute(**kwargs) -> DataFrame:
     def mean_of_ratios(df: DataFrame) -> List[engine.RatioCalculationResult]:
-        working_df = (
-            df.withColumn("growth_ratio_forward", col("output") / col("other_output"))
-            .join(
-                df.select(
-                    col("period").alias("other_period"),
-                    col("grouping").alias("other_grouping"),
-                    col("other_output").alias("reverse_output"),
-                    col("output").alias("reverse_other_output"),
-                ),
-                [
-                    col("next_period") == col("other_period"),
-                    col("grouping") == col("other_grouping"),
-                ],
-                "leftouter",
-            )
-            .select(
-                col("period"),
-                col("grouping"),
-                col("growth_ratio_forward"),
-                (col("reverse_output") / col("reverse_other_output")).alias(
-                    "growth_ratio_backward"
-                ),
-            )
-        )
+        growth_df = df.selectExpr(
+            "period",
+            "grouping",
+            "ref",
+            "current.output/previous.output AS growth_forward",
+            "current.output/next.output AS growth_backward"
 
-        # Calculate the construction links as a ratio of means between the returned values and the auxiliary value
-        construction_df = (
-            df.groupBy("period", "grouping")
-            .agg(
-                sum(col("aux")),
-                sum(col("output_for_construction")),
-                count(col("output_for_construction")),
-            )
-            .withColumn(
-                "construction", col("sum(output_for_construction)") / col("sum(aux)")
-            )
-            .withColumn(
-                "count_construction",
-                when(col("sum(aux)") == 0, 0)
-                .when(
-                    col("sum(aux)").isNotNull(), col("count(output_for_construction)")
-                )
-                .cast("long"),
-            )
-            .join(
-                df.select("period", "grouping").distinct(),
-                ["period", "grouping"],
-            )
         )
 
         return [
             engine.RatioCalculationResult(
-                data=returned_df,
-                join_columns=["period", "grouping"],
-                fill_columns=["forward", "backward", "construction"],
+                data=growth_df,
+                join_columns=["period", "ref", "grouping"],
+                fill_columns=["growth_forward", "growth_backward"]
             )
         ]
 
