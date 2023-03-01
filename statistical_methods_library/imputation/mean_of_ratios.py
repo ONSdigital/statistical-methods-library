c931fc19befb5f4c94999439b6a994084659fd69
Adam Thompson <arthompson1990@gmail.com>
2023-03-01 17:21:43 +0000

    First pass at upper and lower trimming bounds

--- statistical_methods_library/imputation/mean_of_ratios.py
+++ statistical_methods_library/imputation/mean_of_ratios.py
@@ -1,21 +1,45 @@
 # For Copyright information, please see LICENCE.
 
-from typing import List
+from typing import List, Number, Optional
 
 from pyspark.sql import DataFrame
 
 from . import engine
 
 
-def impute(**kwargs) -> DataFrame:
+def impute(*, lower_trim: Optional[Number]=None, upper_trim: Optional[Number]=None, **kwargs) -> DataFrame:
     def mean_of_ratios(df: DataFrame) -> List[engine.RatioCalculationResult]:
         growth_df = df.selectExpr(
             "period",
             "grouping",
             "ref",
-            "current.output/previous.output AS growth_forward",
-            "current.output/next.output AS growth_backward",
-        ).fillna(1.0, "growth_forward", "growth_backward")
+            """CASE
+                WHEN previous.output = 0
+                THEN 1
+                ELSE current.output/previous.output AS growth_forward
+            END""",
+            """CASE
+                WHEN next.output = 0
+                THEN 1
+                ELSE current.output/next.output AS growth_backward
+            END"""
+        )
+
+        trimmed_df = (
+            growth_df.groupBy("period", "grouping")
+            .agg(
+                expr("sum(growth_forward IS NOT NULL) AS count_forward"),
+                expr("sum(growth_backward IS NOT NULL) AS count_backward"),
+            )
+            .select(
+                col("period"),
+                col("grouping"),
+                (1 + (col("count_forward") * int(lower_trim) / 100)).alias("lower_forward"),
+                (col("count_forward") * (100 - int(upper_trim)) / 100).alias("lower_forward"),
+                (1 + (col("count_backward") * int(lower_trim) / 100)).alias("lower_backward"),
+                (col("count_backward") * (100 - int(upper_trim)) / 100).alias("lower_backward"),
+            )
+        )
 
         return [
             engine.RatioCalculationResult(
