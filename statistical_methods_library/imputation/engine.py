4fb2d9191ee806b0a8ffd9fb248bec32bad80132
Adam Thompson <arthompson1990@gmail.com>
2023-03-02 15:44:39 +0000

    New period calculations to support variable periodicity

--- statistical_methods_library/imputation/engine.py
+++ statistical_methods_library/imputation/engine.py
@@ -506,15 +506,14 @@ def impute(
             ]
         )
 
-    def calculate_previous_period(period: Column) -> Column:
-        return when(
-            period.endswith("01"), (period.cast(IntegerType()) - 89).cast(StringType())
-        ).otherwise((period.cast(IntegerType()) - 1).cast(StringType()))
+    def calculate_previous_period(period: Column, num_periods: Optional[int]=1) -> Column:
+        period = period.cast(IntegerType)
+        return (period-(num_periods + (num/_periods/12 + (period % 100 <= num_periods % 12).cast(IntegerType))*88)).cast(StringType)
 
-    def calculate_next_period(period: Column) -> Column:
-        return when(
-            period.endswith("12"), (period.cast(IntegerType()) + 89).cast(StringType())
-        ).otherwise((period.cast(IntegerType()) + 1).cast(StringType()))
+
+    def calculate_next_period(period: Column, num_periods: Optional[int]=1) -> Column:
+        period = period.cast(IntegerType)
+        return (period+ num_periods + ((num_periods//12 + ((period % 100) + (num_periods % 12) > 12).cast(IntegerType))*88)).cast(StringType)
 
     def filter_back_data(filter_col: Column) -> DataFrame:
         return prepared_back_data_df.filter(filter_col).localCheckpoint(eager=True)
