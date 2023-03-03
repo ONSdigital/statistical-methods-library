044fd2e99c2c67592b3a2687c647b663838e64f8
Adam Thompson <arthompson1990@gmail.com>
2023-03-03 14:30:45 +0000

    Mean of ratios method fixes

--- statistical_methods_library/imputation/mean_of_ratios.py
+++ statistical_methods_library/imputation/mean_of_ratios.py
@@ -1,6 +1,7 @@
 # For Copyright information, please see LICENCE.
-
-from typing import List, Number, Optional
+from decimal import Decimal
+from numbers import Number
+from typing import List, Optional
 
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import col, expr, when
@@ -16,11 +17,12 @@ def impute(
     **kwargs
 ) -> DataFrame:
     def mean_of_ratios(df: DataFrame) -> List[engine.RatioCalculationResult]:
-        growth_df = df.selectExpr(
+        working_df = df.selectExpr(
             "period",
             "grouping",
             "ref",
             "aux",
+            "current.output",
             """CASE
                 WHEN previous.output = 0
                 THEN 1
@@ -33,99 +35,125 @@ def impute(
             END AS growth_backward""",
         )
 
+        if lower_trim is not None:
+            trimmed_df = (
+                working_df.join(
+                    (
+                        working_df.groupBy("period", "grouping")
+                        .agg(
+                            expr(
+                                """
+                                sum(
+                                    cast(growth_forward IS NOT NULL AS integer)
+                                ) AS count_forward
+                            """
+                            ),
+                            expr(
+                                """
+                                sum(
+                                    cast(growth_backward IS NOT NULL AS integer)
+                                ) AS count_backward
+                            """
+                            ),
+                        )
+                        .select(
+                            col("period"),
+                            col("grouping"),
+                            (col("count_forward") * Decimal(lower_trim) / 100).alias(
+                                "lower_forward"
+                            ),
+                            (col("count_forward") * (100 - Decimal(upper_trim)) / 100).alias(
+                                "upper_forward"
+                            ),
+                            (col("count_backward") * Decimal(lower_trim) / 100).alias(
+                                "lower_backward"
+                            ),
+                            (col("count_backward") * (100 - Decimal(upper_trim)) / 100).alias(
+                                "upper_backward"
+                            ),
+                        )
+                    ),
+                    ["period", "grouping"],
+                )
+                .withColumn(
+                    "num_forward",
+                    expr(
+                        """
+                        row_number() OVER (
+                            PARTITION BY period, grouping
+                            ORDER BY growth_forward ASC
+                        )
+                    """
+                    ),
+                )
+                .withColumn(
+                    "num_backward",
+                    expr(
+                        """
+                        row_number() OVER (
+                            PARTITION BY period, grouping
+                            ORDER BY growth_backward ASC
+                        )
+                    """
+                    ),
+                )
+                .select(
+                    col("period"),
+                    col("grouping"),
+                    when(
+                        (
+                            col("num_forward").between(
+                                col("lower_forward"), col("upper_forward")
+                            )
+                            | trim_threshold
+                            >= col("count_forward")
+                        ),
+                        col("growth_forward"),
+                    ).alias("trimmed_forward"),
+                    when(
+                        (
+                            col("num_backward").between(
+                                col("lower_backward"), col("upper_backward")
+                            )
+                            | trim_threshold
+                            >= col("count_backward")
+                        ),
+                        col("growth_backward"),
+                    ).alias("trimmed_backward"),
+                )
+            )
+
+        else:
+            trimmed_df = (
+                working_df.withColumn(
+                    "trimmed_forward",
+                    col("growth_forward")
+                )
+                .withColumn(
+                    "trimmed_backward",
+                    col("growth_backward")
+                )
+            )
+
         ratio_df = (
-            growth_df.join(
-                (
-                    growth_df.groupBy("period", "grouping")
-                    .agg(
-                        expr(
-                            """
-                            sum(
-                                cast(growth_forward IS NOT NULL AS integer)
-                            ) AS count_forward
-                        """
-                        ),
-                        expr(
-                            """
-                            sum(
-                                cast(growth_backward IS NOT NULL AS integer)
-                            ) AS count_backward
-                        """
-                        ),
-                    )
-                    .select(
-                        col("period"),
-                        col("grouping"),
-                        (col("count_forward") * decimal(lower_trim) / 100).alias(
-                            "lower_forward"
-                        ),
-                        (col("count_forward") * (100 - decimal(upper_trim)) / 100).alias(
-                            "upper_forward"
-                        ),
-                        (col("count_backward") * decimal(lower_trim) / 100).alias(
-                            "lower_backward"
-                        ),
-                        (col("count_backward") * (100 - decimal(upper_trim)) / 100).alias(
-                            "upper_backward"
-                        ),
-                    )
-                ),
-                ["period", "grouping"],
-            )
-            .withColumn(
-                "num_forward",
-                expr(
-                    """
-                    row_number() OVER (
-                        PARTITION BY period, grouping
-                        ORDER BY growth_forward ASC
-                    )
-                """
-                ),
-            )
-            .withColumn(
-                "num_backward",
-                expr(
-                    """
-                    row_number() OVER (
-                        PARTITION BY period, grouping
-                        ORDER BY growth_backward ASC
-                    )
-                """
-                ),
-            )
-            .select(
-                col("period"),
-                col("grouping"),
-                when(
-                    (
-                        col("num_forward").between(
-                            col("lower_forward"), col("upper_forward")
-                        )
-                        | trim_threshold
-                        >= col("count_forward")
-                    ),
-                    col("growth_forward"),
-                ).alias("trimmed_forward"),
-                when(
-                    (
-                        col("num_backward").between(
-                            col("lower_backward"), col("upper_backward")
-                        )
-                        | trim_threshold
-                        >= col("count_backward")
-                    ),
-                    col("growth_backward"),
-                ).alias("trimmed_backward"),
-            )
-            .groupBy("period", "grouping")
+            trimmed_df.groupBy("period", "grouping")
             .agg(
                 expr("mean(trimmed_forward) AS forward"),
                 expr("mean(trimmed_backward) AS backward"),
                 expr("sum(current.output)/sum(aux) AS construction"),
+                expr("count(trimmed_forward) AS count_forward"),
+                expr("count(trimmed_backward) AS count_backward"),
+                expr("count(current.output) AS count_construction"),
             )
         )
 
+        growth_df = working_df.select(
+            "ref",
+            "period",
+            "grouping",
+            "growth_forward",
+            "growth_backward"
+        )
         return [
             engine.RatioCalculationResult(
                 data=growth_df,
