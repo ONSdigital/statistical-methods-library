94eef4ed6f11ea3ae176607519f4f0b91325aaf2
Adam Thompson <arthompson1990@gmail.com>
2023-04-12 12:41:33 +0100

    Removed unused import

--- statistical_methods_library/outliering/winsorisation.py
+++ statistical_methods_library/outliering/winsorisation.py
@@ -10,7 +10,7 @@ from enum import Enum
 
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import col, expr, lit, when
-from pyspark.sql.types import DecimalType, StringType
+from pyspark.sql.types import DecimalType
 
 from statistical_methods_library.utilities import validation
 
